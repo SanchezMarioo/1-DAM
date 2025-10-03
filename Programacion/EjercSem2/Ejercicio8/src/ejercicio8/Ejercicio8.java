7483e160c77216ea1379d753ac7cc6a89f0154d6
Mario Sanchez <mariosp860@gmail.com>
2025-10-03 17:52:43 +0200

    ADD: Add ejercicios7-9

--- Programacion/EjercSem2/Ejercicio8/src/ejercicio8/Ejercicio8.java
+++ Programacion/EjercSem2/Ejercicio8/src/ejercicio8/Ejercicio8.java
@@ -2,30 +2,29 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package ejercicio7;
+package ejercicio8;
 
 import java.util.Scanner;
 
 /**
  *
- * @author mario.sanper.2
+ * @author Mario
  */
-public class Ejercicio7 {
+public class Ejercicio8 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        float numero;
+        float notaProgramacion;
         Scanner sc = new Scanner (System.in);
-        System.out.print("Introduce el numero casi/cero: ");
-        numero = sc.nextFloat();
-        
-        if (numero != 0 && numero < 1 && numero > -1) {
-            System.out.println("Numero casi cero ");
+        System.out.print("Que nota tienes en progamacion: ");
+        notaProgramacion = sc.nextFloat();
+        if (notaProgramacion >= 5) {
+            System.out.println("¡Enhorabuena has aprobado!");
         } else {
-            System.out.println("No es numero casi cero");
+            System.out.println("Venga! Tienes que seguir intentándolo");
         }
         
         
---
+++ Programacion/EjercSem2/Ejercicio9/src/ejercicio9/Ejercicio9.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio9;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Mario
+ */
+public class Ejercicio9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int edad;
+        boolean entradaVIP,hijoArtistaInvitado;
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Cuantos años tienes: ");
+        edad = sc.nextInt();
+        System.out.print("Tienes entrada VIP (true/false): ");
+        entradaVIP = sc.nextBoolean();
+        System.out.print("Eres hijo del artista invitado (true/false): ");
+        hijoArtistaInvitado = sc.nextBoolean();
+        
+        
+        
+        if ((edad >= 18 && entradaVIP) || hijoArtistaInvitado ){
+            System.out.println("Puedes pasar");
+        } else {
+            System.out.println("No puedes pasar");
+        }
+    }
+    
+}

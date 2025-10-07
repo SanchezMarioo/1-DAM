1b0c366d52128b65e8f76c3a67e78828c14edc8c
Mario Sanchez <mariosp860@gmail.com>
2025-10-07 13:25:49 +0200

    ADD:Exercise 14

---
+++ Programacion/EjercSem2/Ejercicio13/src/ejercicio13/Ejercicio13.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio13;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author mario.sanper.2
+ */
+public class Ejercicio13 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a,b,c;
+        double ecuacionPos,ecuacionNeg = 0;
+        double raizCuadrada;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Dime la letra A:");
+        a = sc.nextInt();
+        System.out.println("Dime la letra B:");
+        b = sc.nextInt();
+        System.out.println("Dime la letra C:");
+        c = sc.nextInt();
+        raizCuadrada = Math.pow(b, 2) - (4 * a * c);
+        
+        if(c == 0 || raizCuadrada <= 0){
+            System.out.println("No se puede hacer");
+        }
+        else{
+            ecuacionPos = (-b + Math.sqrt(raizCuadrada)) / (2 * a);
+            ecuacionNeg = (-b - Math.sqrt(raizCuadrada)) / (2 * a) ;
+            System.out.println(ecuacionPos + " " + raizCuadrada + " " + ecuacionNeg);
+        }
+        System.out.println(raizCuadrada);
+        
+        
+    }
+    
+}

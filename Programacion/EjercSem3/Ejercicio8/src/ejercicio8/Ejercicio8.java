61ae05b0b85cf2abb507a814a7aac6b36e74a74d
Your Name <you@example.com>
2025-10-20 20:43:41 +0200

    ADD: Ejercicio 7,8

--- Programacion/EjercSem3/Ejercicio8/src/ejercicio8/Ejercicio8.java
+++ Programacion/EjercSem3/Ejercicio8/src/ejercicio8/Ejercicio8.java
@@ -17,12 +17,36 @@ public class Ejercicio8 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int eleccion = -1, suma = 0;
+        int respClienteRendir;
+        boolean salir = false;
         Scanner sc = new Scanner(System.in);
-        double numeroRandom;
-        numeroRandom = (Math.random() * 100);
-        int numeroRandomT = (int) numeroRandom;
-        System.out.println(numeroRandomT);
-        
-    }
+        while (salir != true) {
+            double numeroRandom;
+            numeroRandom = (Math.random() * 100);
+            int numeroRandomT = (int) numeroRandom;
+            System.out.println("El numero random es: " + numeroRandomT);
+            System.out.println("Quieres otro numero pulsa 1");
+            System.out.println("Si quieres rendirte pulsa 0");
+            eleccion = sc.nextInt();
+            System.out.println("Elige una opcion:");
+            suma += numeroRandomT;
+            switch (eleccion) {
+                case 0:
+                    System.out.println("¿Cuál crees que es la suma de los números?");
+                    respClienteRendir = sc.nextInt();
+                    if (respClienteRendir == suma) {
+                        System.out.println("Lo acertaste la suma es " + suma);
+                    } else {
+                        System.out.println("Lo siento fallaste" + suma);
+                    }
+                    salir = true;
+                    break;
+                default:
+                    System.out.println("No has introducido el numero correcto");
 
+            }
+        }
+
+    }
 }

f5732954b6ffbe42f96faed03e3c2f2d0b1bba5d
Tu Nombre <tuemail@ejemplo.com>
2026-01-09 18:15:25 +0100

    ADD: Ex 23

--- Programacion/EjercSem5/Ejercicio23/src/ejercicio23/Ejercicio23.java
+++ Programacion/EjercSem5/Ejercicio23/src/ejercicio23/Ejercicio23.java
@@ -16,23 +16,125 @@ public class Ejercicio23 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        boolean[][] parking = new boolean[5][3];
         Scanner sc = new Scanner(System.in);
+        // TODO code application logic here
+        boolean[][] parking = new boolean[3][5];
         do {
-            System.out.println("===== GESTIÓN DEL PARKING =====");
-            System.out.println("1. Aparcar un coche");
-            System.out.println("2. Sacar un coche");
-            System.out.println("3. Mostrar estado del parking");
-            System.out.println("4. Salir del programa");
-            System.out.print("Selecciona una opción: ");
-            int opcion = sc.nextInt();
-            switch (opcion){
+            estadoPlazas(parking);
+            siguentePlaza(parking);
+            int opcion = mostrarMenu(sc);
+            if (opcion == 4) {
+                break;
+            }
+            switch (opcion) {
                 case 1 -> {
-                    
+                    aparcarCoche(sc, parking);
+                }
+                case 2 -> {
+                    sacarCoche(sc, parking);
+                }
+                case 3 -> {
+                    estadoParking(parking);
+                }
+                default -> {
+                    System.out.println("Opcion no valida.");
                 }
             }
         } while (true);
     }
-    static 
+
+    static int mostrarMenu(Scanner sc) {
+        System.out.println("===== GESTIÓN DEL PARKING =====");
+        System.out.println("1. Aparcar un coche");
+        System.out.println("2. Sacar un coche");
+        System.out.println("3. Mostrar estado del parking");
+        System.out.println("4. Salir del programa");
+        System.out.print("Selecciona una opción: ");
+        int opcion = sc.nextInt();
+        return opcion;
+    }
+
+    static int aparcarCoche(Scanner sc, boolean[][] parking) {
+        int contadorOcupados = 0;
+        System.out.print("Introduce la planta (0-2): ");
+        int planta = sc.nextInt();
+        System.out.print("Introduce el número de plaza (0-4): ");
+        int numeroPlanta = sc.nextInt();
+        int numeroPlaza = 3 * 5;
+        if (contadorOcupados == numeroPlaza) {
+            System.out.println("El parking está completo. No se pueden aparcar más coches.");
+        }
+        if ((planta < 0 || planta > 3) || (numeroPlanta < 0 || numeroPlanta > 5)) {
+            System.out.println("Datos incorrectos. Planta o plaza no válida.");
+        }
+        if (parking[planta][numeroPlanta] == true) {
+            System.out.println("No es posible aparcar: la plaza está ocupada.");
+        } else {
+            parking[planta][numeroPlanta] = true;
+            System.out.println("Coche aparcado correctamente.");
+        }
+        return contadorOcupados;
+    }
+
+    static void sacarCoche(Scanner sc, boolean[][] parking) {
+        System.out.print("Introduce la planta (0-2): ");
+        int planta = sc.nextInt();
+        System.out.print("Introduce el número de plaza (0-4): ");
+        int numeroPlanta = sc.nextInt();
+        if ((planta < 0 || planta > 2) || (numeroPlanta < 0 || numeroPlanta > 4)) {
+            System.out.println("Datos incorrectos. Planta o plaza no válida.");
+        }
+        if (parking[planta][numeroPlanta] == true) {
+            parking[planta][numeroPlanta] = false;
+            System.out.println("Coche retirado correctamente.");
+        } else {
+            System.out.println("No hay ningún coche en esa plaza.");
+        }
+    }
+
+    static void estadoParking(boolean[][] parking) {
+        System.out.println("===== ESTADO DEL PARKING =====");
+        for (int i = 0; i < 3; i++) {
+            System.out.println("\nPlanta" + (i) + "\n");
+            for (int j = 0; j < 5; j++) {
+                if (parking[i][j] == true) {
+                    System.out.println("Plaza " + (j) + " ocupada");
+                } else {
+                    System.out.println("Plaza " + (j) + " libre");
+                }
+            }
+        }
+    }
+
+    static void estadoPlazas(boolean[][] parking) {
+        int plazasOcupadas = 0;
+        int plazasLibres = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (parking[i][j] == true) {
+                    plazasOcupadas++;
+                } else {
+                    plazasLibres++;
+                }
+            }
+        }
+        System.out.println("Plazas ocupadas: " + plazasOcupadas);
+        System.out.println("Plazas libres: " + plazasLibres);
+    }
+
+    static void siguentePlaza(boolean[][] parking) {
+        boolean noEntre = false;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (!noEntre) {
+                    if (parking[i][j] == false) {
+                        noEntre = true;
+                        System.out.println("Primera plaza libre disponible en planta " + (i) + ", plaza " + (j));
+                        break;
+                    }
+                }
+            }
+        }
+
+    }
 }

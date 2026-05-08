1f1bf69a67259c7db0c9c18350a418ae6a4c7c84
MARIO SANCHEZ PEREZ <mario.sanper.2@educa.jcyl.es>
2026-05-08 10:09:45 +0200

    a

--- Lenguajes de Marcas/JavaScript/Ejer4/index.js
+++ Lenguajes de Marcas/JavaScript/Ejer4/index.js
@@ -1,7 +1,18 @@
 let numero1 = Number(prompt("Introduce el numero: "));
 let numero2 = Number(prompt("Introduce el numero: "));
-
-document.write(numero1 + " + " + numero2 +" = " + (numero1 + numero2) + "<br>");
-document.write(numero1 + " - " + numero2 +" = " + (numero1 - numero2) + "<br>");
-document.write(numero1 + " * " + numero2 +" = " + (numero1 * numero2) + "<br>");
-document.write(numero1 + " / " + numero2 +" = " + (numero1 / numero2) + "<br>");
+if (!isNaN(numero1) && !isNaN(numero2)) {
+  document.writeln(
+    "<p>" + numero1 + " + " + numero2 + " = " + (numero1 + numero2) + "</p>"
+  );
+  document.writeln(
+    "<p>" + numero1 + " - " + numero2 + " = " + (numero1 - numero2) + "</p>"
+  );
+  document.write(
+    "<p>" + numero1 + " * " + numero2 + " = " + numero1 * numero2 + "</p>"
+  );
+  document.write(
+    "<p>" + numero1 + " / " + numero2 + " = " + numero1 / numero2 + "</p>"
+  );
+} else{
+    document.writeln("<p>Esto no es un numero</p>")
+}

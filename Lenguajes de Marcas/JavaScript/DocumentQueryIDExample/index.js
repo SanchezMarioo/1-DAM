4755115475350715835edce6ad9039bb87fdb737
MARIO SANCHEZ PEREZ <mario.sanper.2@educa.jcyl.es>
2026-05-13 12:06:52 +0200

    ADD:Intento ejercicico lenguajes

--- Lenguajes de Marcas/JavaScript/DocumentQueryIDExample/index.js
+++
@@ -1,3 +0,0 @@
-let elemento = document.getElementById("prueba")
-let texto = prompt("Dime el titulo: ")
-elemento.innerHTML = texto
--- Lenguajes de Marcas/JavaScript/EjerBN/index.js
+++ Lenguajes de Marcas/JavaScript/EjerBN/index.js
@@ -1,6 +1,9 @@
-let img = document.getElementById("color");
-while (true) {
-    let msg = prompt("Escribe BN o color: ")
+let img = document.getElementById("img");
+console.log(img)
+let msg;
+while (msg !== "BN" && msg !== "color" && msg !== "COLOR") {
+    msg = prompt("Escribe BN o color: ")
+    console.log(msg)
     if (msg === "BN") {
         img.src = "img/imagenBN.jpg"
         img.alt="Imagen a color"
@@ -8,5 +11,7 @@ while (true) {
     } else if (msg === "color" || msg === "COLOR"){
         img.src = "img/imagenColor.jpg"
         break;
+    } else{
+        msg = prompt("Escribe BN o color: ") 
     }
 }

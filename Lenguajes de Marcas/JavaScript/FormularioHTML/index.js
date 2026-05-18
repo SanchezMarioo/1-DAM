f0a41d5287de49ce4ac047511bf2d00b9230d235
MARIO SANCHEZ PEREZ <mario.sanper.2@educa.jcyl.es>
2026-05-18 12:42:34 +0200

    ADD:Formulario validacion Javascript

---
+++ Lenguajes de Marcas/JavaScript/FormularioHTML/index.js
@@ -0,0 +1,31 @@
+let esCorrecto;
+let registro = document.getElementById("registro");
+console.log(registro);
+
+registro.addEventListener("click", error);
+function error() {
+  let email = document.getElementById("email");
+  let nombre = document.getElementById("nombre");
+  let contrasena = document.getElementById("password");
+  let container = document.getElementById("form-container");
+  let char = "@";
+  let punto = ".";
+  esCorrecto = true;
+
+  if (nombre.value.length < 4) {
+    esCorrecto = false;
+    nombre.classList.add("error");
+  }
+  if (!email.value.includes(char) || !email.value.includes(punto)) {
+    email.classList.add("error");
+    esCorrecto = false;
+  }
+  console.log(contrasena.value);
+  if (contrasena.value !== "1234") {
+    contrasena.classList.add("error");
+    esCorrecto = false;
+  }
+  if (esCorrecto) {
+    container.style.display = "none";
+  }
+}

778f0c4864c9608d3aa71b11590dcb7b161f801c
Mario sanchez <mariosp860@gmail.com>
2026-05-22 19:13:39 +0200

    ADD: Ejer 8

---
+++ Programacion/EjerSem10/Ejercicio8/src/ConexionBD/ConexionBD.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ConexionBD;
+import java.sql.*;
+/**
+ *
+ * @author Mario
+ */
+public class ConexionBD {
+    private static final String URL = "jdbc:mysql://localhost:3306/DBEjemplo";
+    private static final String USER = "root";
+    private static final String PASSWORD = "ejemplo1234";
+    public static Connection connect() throws SQLException{
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
---
+++ Programacion/EjerSem10/Ejercicio8/src/Dao/FicheroDAO.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Dao;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author Mario
+ */
+public class FicheroDAO {
+
+    private static final String ruta = "ejemplo.txt";
+    private static final String REGEXP = "[A-Za-z0-9\\._\\-+]+@[A-Za-z0-9\\._\\-+]\\.[A-Za-z]{2,}";
+    private Pattern pattern;
+    private Matcher matcher;
+
+    public ArrayList<String> leer() throws FileNotFoundException, IOException {
+        ArrayList<String> correos = new ArrayList<>();
+        pattern = Pattern.compile(REGEXP);
+        String linea;
+        try (BufferedReader lector = new BufferedReader(new FileReader(ruta))) {
+            while ((linea = lector.readLine()) != null) {
+                matcher = pattern.matcher(linea);
+                while(!matcher.find()){
+                    matcher.group();
+                    correos.add(linea);
+                }
+                
+            }
+        }
+        return correos;
+    }
+}

36c5760ba1bba91e4ddcfb475b4ce8aecbd7e405
Mario Sanchez+ <mariosp860@gmail.com>
2026-05-28 09:22:39 +0200

    ADD:Tres en raya

--- Lenguajes de Marcas/JavaScript/TresEnRaya/js/script.js
+++ Lenguajes de Marcas/JavaScript/TresEnRaya/js/script.js
@@ -1,33 +1,32 @@
 let tablero = document.getElementsByClassName("tresRayaTablero");
+let turnoTitulo;
 let turno = "X";
 let posiciones = ["", "", "", "", "", "", "", "", ""];
+const jugador1 = "X";
+const jugador2 = "O";
 window.onload = function () {
-  let turnoTitulo = document.getElementById("tituloTurno");
-  jugar()
-  let btn = document.getElementById("btn-reinciar")
-  btn.addEventListener("click", limpiarTablero)
-
-
+  turnoTitulo = document.getElementById("tituloTurno");
+  jugar();
+  let btn = document.getElementById("btn-reinciar");
+  btn.addEventListener("click", limpiarTablero);
 };
 
 function jugar() {
   let turnoTitulo = document.getElementById("tituloTurno");
   turno = cambiarTurno(turno);
-  turnoTitulo.textContent = "Turno de " + turno
+  turnoTitulo.textContent = "Turno de " + turno;
   for (let i = 0; i < tablero.length; i++) {
     tablero[i].addEventListener("click", function () {
       if (!comprobarGanador() && tablero[i].textContent == "") {
-        turnoTitulo.textContent = "Turno de " + turno
+        turnoTitulo.textContent = "Turno de " + turno;
         turno = cambiarTurno(turno);
         tablero[i].textContent = turno;
         posiciones[i] = turno;
         comprobarEmpate();
-        comprobarGanador(turnoTitulo);
+        comprobarGanador();
       }
-
     });
   }
-
 }
 
 function cambiarTurno(turno) {
@@ -41,78 +40,101 @@ function limpiarTablero() {
   for (let i = 0; i < tablero.length; i++) {
     // Limpiar el tablero de texto
     tablero[i].textContent = "";
-    // Quitamos lo verde en  caso de haber ganado 
-    tablero[i].classList.remove("verde")
+    // Quitamos lo verde en  caso de haber ganado
+    tablero[i].classList.remove("verde");
     // Limpiamos el array
-    posiciones = ["", "", "", "", "", "", "", "", ""]
+    posiciones = ["", "", "", "", "", "", "", "", ""];
   }
 }
-// En caso de estar lleno el tablero se da empate 
+// En caso de estar lleno el tablero se da empate
 function comprobarEmpate() {
   let turnoEmpate = document.getElementById("tituloTurno");
   for (let i = 0; i < posiciones.length; i++) {
     if (posiciones[i] == "") {
       return true;
     }
-
   }
-  turnoEmpate.textContent = "El juego ha quedado empate."
+  turnoEmpate.textContent = "El juego ha quedado empate.";
   return false;
 }
-
-function comprobarGanador(tituloGanador) {
-  // Horizontales
-  if ((posiciones[0] === "X" && posiciones[1] === "X" && posiciones[2] === "X") || (posiciones[0] === "O" && posiciones[1] === "O" && posiciones[2] === "O")) {
-    let posiciones = [0, 1, 2];
-    esGanador(posiciones, tituloGanador)
-    return true;
-
-  } else if ((posiciones[3] === "X" && posiciones[4] === "X" && posiciones[5] === "X") || (posiciones[3] === "O" && posiciones[4] === "O" && posiciones[5] === "O")) {
-    let posiciones = [3, 4, 5];
-    esGanador(posiciones, tituloGanador)
-    return true;
-  }
-  else if ((posiciones[6] === "X" && posiciones[7] === "X" && posiciones[8] === "X") ||
-    (posiciones[6] === "O" && posiciones[7] === "O" && posiciones[8] === "O")) {
-    let posiciones = [6, 7, 8];
-    esGanador(posiciones, tituloGanador)
-    return true;
-  }
-  // Verticales
-  else if ((posiciones[0] === "X" && posiciones[3] === "X" && posiciones[6] === "X") || posiciones[0] === "O" && posiciones[3] === "O" && posiciones[6] === "O") {
-    let posiciones = [0, 3, 6];
-    esGanador(posiciones, tituloGanador)
-    return true;
-  }
-  else if ((posiciones[2] === "X" && posiciones[5] === "X" && posiciones[8] === "X") || posiciones[2] === "O" && posiciones[5] === "O" && posiciones[8] === "O") {
-    let posiciones = [2, 5, 8];
-    esGanador(posiciones, tituloGanador)
-    return true;
-  }
-  else if ((posiciones[1] === "X" && posiciones[4] === "X" && posiciones[7] === "X") || posiciones[1] === "O" && posiciones[4] === "O" && posiciones[7] === "O") {
-    let posiciones = [1, 4, 7];
-    esGanador(posiciones, tituloGanador)
-    return true;
-  }
-  // Diagonales
-  else if ((posiciones[0] === "X" && posiciones[4] === "X" && posiciones[8] === "X") ||
-    (posiciones[0] === "O" && posiciones[4] === "O" && posiciones[8] === "O")) {
-    let posiciones = [0, 4, 8];
-    esGanador(posiciones, tituloGanador)
-    return true;
-  }
-  else if ((posiciones[2] === "X" && posiciones[4] === "X" && posiciones[6] === "X") ||
-    (posiciones[2] === "O" && posiciones[4] === "O" && posiciones[6] === "O")) {
-    let posiciones = [2, 4, 6];
-    esGanador(posiciones, tituloGanador)
-    return true;
-  }
-  return false;
-}
-function esGanador(posiciones, tituloGanador) {
+function contatenarArray(posiciones, cadena) {
   for (let i = 0; i < posiciones.length; i++) {
-    tablero[posiciones[i]].classList.add("verde");
-
+    if (tablero[posiciones[i]].textContent !== cadena) {
+      return false;
+    }
   }
-  tituloGanador.textContent = "Ha ganado el jugador " + tablero[posiciones[0]].textContent
+  return true;
+}
+function comprobarGanadorHorizontal() {
+  let horizontales = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+  ];
+  if (contatenarArray(horizontales[0], jugador1) || contatenarArray(horizontales[0], jugador2) ) {
+    esGanador(horizontales[0]);
+    return true;
+  } else if (contatenarArray(horizontales[1], jugador1) ||contatenarArray(horizontales[1], jugador2)) {
+    esGanador(horizontales[1]);
+    return true;
+  } else if ((contatenarArray(horizontales[2], jugador1) ||contatenarArray(horizontales[2], jugador2))) {
+    esGanador(horizontales[2]);
+    return true;
+  }
+  return false;
+}
+function comprobarGanadorVertical() {
+  // Verticales
+  let verticales = [
+    [0, 3, 6], 
+    [2, 5, 8],
+    [1, 4, 7]
+  ];
+  if ((contatenarArray(verticales[0], jugador1) || contatenarArray(verticales[0], jugador2))) {
+    esGanador(verticales[0]);
+    return true;
+  } else if ((contatenarArray(verticales[1], jugador1) || contatenarArray(verticales[1], jugador2))) {
+    esGanador(verticales[1]);
+    return true;
+  } else if((contatenarArray(verticales[2], jugador1) || contatenarArray(verticales[2], jugador2)))  {
+    esGanador(verticales[2]);
+    return true;
+  }
+  return false;
+}
+function comprobarGanadorDiagonal() {
+  let diagonales = [
+    [0,4,8],
+    [2,4,6]
+  ]
+  // Diagonales
+  if ((contatenarArray(diagonales[0]),jugador1) || (contatenarArray(diagonales[0]),jugador2)) {
+    esGanador(diagonales[0]);
+    return true;
+  } else if ((contatenarArray(diagonales[1]),jugador1) || (contatenarArray(diagonales[1]),jugador2)) {
+    esGanador(diagonales[1]);
+    return true;
+  }
+  return false;
+}
+function comprobarGanador() {
+  // Horizontales
+  if (
+    comprobarGanadorHorizontal() ||
+    comprobarGanadorVertical() 
+    //comprobarGanadorDiagonal()
+   ) {
+    return true;
+  }
+  console.log("entra");
+  return false;
+}
+function esGanador(posiciones) {
+  for (let i = 0; i < posiciones.length; i++) {
+    console.log(tablero[posiciones[i]])
+    tablero[posiciones[i]].classList.add("verde");
+  }
+  console.log(tablero[posiciones[0]].textContent)
+  turnoTitulo.textContent =
+    "Ha ganado el jugador " + tablero[posiciones[0]].textContent;
 }
